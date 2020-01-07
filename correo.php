5cbc365b476ba2f7897ee7c943e20893181015af
Oscar de La Rosa <oscardelarosa100@gmail.com>
2020-01-07 03:18:31 -0600

    v3: En esta versión ya esta configurado el formulario

--- correo.php
+++ correo.php
@@ -2,17 +2,22 @@
 
 if (isset($_POST['enviar'])){
     if(!empty($_POST['name']) && !empty($_POST['email']) && !empty($_POST['phone']) && !empty($_POST['msg'])){
-        $name= $_POST['name'];
-        $asunto = $_POST['phone'];
-        $msg = $_POST['msg'];
-        $email = $_POST['email'];
-        $header = "From: noreply@example.com" . "\r\n";
-        $header.= "Reply-To: noreply@example.com" ."\r\n";
-        $header.= "X-Mailer: PHP/". phpversion();
-        $mail = @mail($email,$asunto,$msg,$header);
+
+        $destino ="sthartutorias@gmail.com";
+        $nombre = $_POST['name'];
+        $correo = $_POST['email'];
+        $telefono = $_POST['phone'];
+        $mensaje = $_POST['msg'];
+        $contenido = "Nombre: " . $nombre . "\nCorreo: " . $correo . "\nTeléfono: " . $telefono . "\nMensaje: " . $mensaje;
+        $mail = mail($destino, "Contacto", $contenido);
+
         if ($mail){
             echo "Su mensaje ha sido enviado";
         }
     }
 
 }
+
+
+
+    
